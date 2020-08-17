0fa6a0e767d060c894e590d2cde0012ad9c890bd
lanqiu <ksumebosi@gmail.com>
2020-08-17 20:06:17 +0900

    Hello, World

---
+++ src/main/main.ino
@@ -0,0 +1,40 @@
+#include <M5Stack.h>
+
+// the setup routine runs once when M5Stack starts up
+void setup(){
+
+  // Initialize the M5Stack object
+  M5.begin();
+
+  /*
+    Power chip connected to gpio21, gpio22, I2C device
+    Set battery charging voltage and current
+    If used battery, please call this function in your project
+  */
+  M5.Power.begin();
+    
+  // LCD display
+  M5.Lcd.print("Hello World");
+}
+
+// the loop routine runs over and over again forever
+void loop() {
+  
+  M5.update();
+  if (M5.BtnA.wasPressed()){
+    M5.Lcd.clear();
+    M5.Lcd.setCursor(0,0);
+    M5.Lcd.print("Button A");
+  }
+  if (M5.BtnB.wasPressed()){
+    M5.Lcd.clear();
+    M5.Lcd.setCursor(0,0);
+    M5.Lcd.print("Button B");
+  }
+  if (M5.BtnC.wasPressed()){
+    M5.Lcd.clear();
+    M5.Lcd.setCursor(0,0);
+    M5.Lcd.print("Button C");
+  }
+
+}
