70373c09e1276e1a763a0c13f37de45f6bcbab75
Masaya Oura <mairu0514m.o@gmail,com>
2020-08-23 08:35:22 +0900

    HTTPサーバの基礎を作成

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,40 +1,87 @@
 #include <M5Stack.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
 
-// the setup routine runs once when M5Stack starts up
-void setup(){
+// wifi settings
+const char* ssid = "********";
+const char* password = "********";
 
-  // Initialize the M5Stack object
-  M5.begin();
+const char* domain = "m5stack";
 
-  /*
-    Power chip connected to gpio21, gpio22, I2C device
-    Set battery charging voltage and current
-    If used battery, please call this function in your project
-  */
-  M5.Power.begin();
-    
-  // LCD display
-  M5.Lcd.print("Hello World");
+WebServer server(80);
+
+const int led = 13;
+
+void handleRoot() {
+  digitalWrite(led, 1);
+  server.send(200, "text/plain", "hello from m5stack!");
+  digitalWrite(led, 0);
+  M5.Lcd.println("Hello");
 }
 
-// the loop routine runs over and over again forever
-void loop() {
-  
-  M5.update();
-  if (M5.BtnA.wasPressed()){
-    M5.Lcd.clear();
-    M5.Lcd.setCursor(0,0);
-    M5.Lcd.print("Button A");
+void handleNotFound() {
+  digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++){
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
   }
-  if (M5.BtnB.wasPressed()){
-    M5.Lcd.clear();
-    M5.Lcd.setCursor(0,0);
-    M5.Lcd.print("Button B");
+  server.send(404, "text/plain", message);
+  digitalWrite(led, 0);
+  M5.Lcd.println("Not Found");
+}
+
+void setup(){
+  M5.begin();
+  M5.Power.begin();
+
+  // WiFi preparation
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 0);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  //Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
-  if (M5.BtnC.wasPressed()){
-    M5.Lcd.clear();
-    M5.Lcd.setCursor(0,0);
-    M5.Lcd.print("Button C");
+  Serial.println("");
+  Serial.print("connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if(MDNS.begin(domain)) {
+    Serial.println("MDNS responder started");
   }
 
+  server.on("/", handleRoot);
+
+  server.on("/inline", []() {
+    server.send(200, "text/plain", "this works as well");
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+  
+  // LCD display
+  M5.Lcd.setTextSize(3);
+  M5.Lcd.println("Waiting");
+}
+
+void loop() {
+  server.handleClient();
 }
